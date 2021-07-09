7b76d756d6e6fe4811182418c578d6e177ee9f87
lukagabi <85452288+lukagabi@users.noreply.github.com>
2021-07-09 06:14:51 -0700

    dd

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-let name = "LUKA";
-let surname = "GOGOCHURI";
+let name = "luka";
+let surname ="gogochuri";
+let r = 10;
+let area = r * r;
+let l = 2 * r;
+ 
+console.log( r * r);
+ console.log( area);
+ console.log(l);
 
-console.log (name + " " + surname);
-let	r = 17;
-let S = Math.PI * r * r;
-let	l = 2 * Math.PI * r;
-console.log	( r * r);
-console.log	( area );
-console.log	(l);
-let x = 3;
-let y = 5;
-let c = Math.sqrt (x * x + y * y);
-console.log ("ჰიპოტენუზა = " + c);
-let	a = prompt("კუბის მოცულობის გამოსათვლელად შეიყვანე წიბოს ზომა");
-console.log ("კუბის მოცულობა = " + Math.pow (a, 3)); 
+ let a = 5;
+ let b = 10;
+ let c = (a * a + b * b);
+
+ console.log("Hypotenuse =" + c);
+
+
+let x = prompt;
+
+console.log ("The volume of the cube =" + (x, 3));
